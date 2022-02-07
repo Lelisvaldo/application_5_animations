ae330cfac543e8678e92a1bb4ff37c5ba8e0f0a6
lelisvaldo <lelisvaldo@outlook.com>
2022-02-06 22:02:35 -0300

    implements AnimatedCrossFade.

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isLoading = !isLoading;
+            });
+          },
+          child: AnimatedContainer(
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 800),
+            height: 80,
+            width: isLoading ? 80 : 350,
+            color: Colors.red,
+            alignment: Alignment.center,
+            child: AnimatedCrossFade(
+              duration: const Duration(seconds: 1),
+              firstChild: Padding (
+                padding: EdgeInsets.all(20),
+                child: CircularProgressIndicator(),
+              ),
+              secondChild: Text(
+                "ENTRAR",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              crossFadeState: isLoading
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
